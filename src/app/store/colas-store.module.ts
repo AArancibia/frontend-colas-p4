9b9adbc3018138dd198d72f27c5420a370125e9d
Alexis Arancibia <alexis2396@hotmail.com>
2019-12-31 16:22:02 -0500

    Frontend Colas to Amazon Web Services

--- src/app/store/colas-store.module.ts
+++ src/app/store/colas-store.module.ts
@@ -1,25 +1,25 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import {StoreDevtoolsModule} from '@ngrx/store-devtools';
-import {EffectsModule} from '@ngrx/effects';
-import {ActionReducerMap, StoreModule} from '@ngrx/store';
-import {environment} from '@env/environment';
-import {errorReducer, ErrorState} from '@app/store/reducers/error.reducer';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { StoreDevtoolsModule } from "@ngrx/store-devtools";
+import { EffectsModule } from "@ngrx/effects";
+import { ActionReducerMap, StoreModule } from "@ngrx/store";
+import { environment } from "@env/environment";
+import { errorReducer, ErrorState } from "@app/store/reducers/error.reducer";
 
 export interface AppState {
-  error: ErrorState;
+  //error: ErrorState;
 }
 
-export const reducers: ActionReducerMap< AppState > = {
-  error: errorReducer,
+export const reducers: ActionReducerMap<AppState> = {
+  //error: errorReducer,
 };
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-    StoreModule.forRoot( reducers ),
-    StoreDevtoolsModule.instrument(),
+    StoreModule.forRoot(reducers),
+    StoreDevtoolsModule.instrument()
   ]
 })
-export class ColasStoreModule { }
+export class ColasStoreModule {}
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
-  url_server: 'http://192.168.10.11:4000/api',
-  wsUrl: 'http://localhost:8081',
+  url_server: "http://18.212.167.46:4052/api/",
+  url_server_tupa: "http://18.212.167.46:4053/api/", //207:4520
+  wsUrl: "http://18.212.167.46:8081"
 };
